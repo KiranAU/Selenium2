3f4205191f9500ec4dace8108e99a023dcbc3c19
Kiran Uttarkar <kiranuttarkar5@gmail.com>
2022-06-10 11:11:46 -0700

    Update sele2.py

--- sele2.py
+++ sele2.py
@@ -1,3 +1,19 @@
+
+ with sc.Transform(transform=sc.Matrix44.get_translation_matrix(*self.model.position.value)):
+            #Label
+            with sc.Transform(look_at=sc.Transform.LookAt.CAMERA):
+                sc.Arc(self._radius, axis=2, color=cl.yellow)
+                sc.Line([0,0,0], [0, self._radius_hovered, 0], color=cl.yellow, thickness=self._thickness)
+                sc.Line([0,self._radius_hovered,0], [self._radius_hovered, self._radius_hovered * 1.5, 0], color=cl.yellow, thickness=self._thickness)
+                
+                with sc.Transform(transform=sc.Matrix44.get_translation_matrix(self._radius_hovered + 5, self._radius_hovered *1.5, self._radius_hovered)):
+                     with sc.Transform(scale_to=sc.Space.SCREEN):
+                        with sc.Transform(transform=sc.Matrix44.get_translation_matrix(0,5,0)):
+                            sc.Label(f"Tooltip:- the prim path is {self.model._current_path}.", color=cl.white, size=15)
+
+
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
